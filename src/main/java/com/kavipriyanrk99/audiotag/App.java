cfe5674ae265f01fb4a87098e39b12bda771e312
kavipriyanrk99 <kavipriyanrk99@gmail.com>
2024-11-11 20:43:48 +0530

    mp3agic dependency added

---
+++ src/main/java/com/kavipriyanrk99/audiotag/App.java
@@ -0,0 +1,10 @@
+package com.kavipriyanrk99.audiotag;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
