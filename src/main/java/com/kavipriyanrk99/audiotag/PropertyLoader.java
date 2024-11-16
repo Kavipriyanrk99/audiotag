af1f16e1b4b271c1b6eef598bd00dd0e024b57bd
kavipriyanrk99 <kavipriyanrk99@gmail.com>
2024-11-16 17:44:18 +0530

    api, auth urls getters setters added

--- src/main/java/com/kavipriyanrk99/audiotag/PropertyLoader.java
+++ src/main/java/com/kavipriyanrk99/audiotag/PropertyLoader.java
@@ -9,41 +9,67 @@ import java.util.Properties;
  */
 public class PropertyLoader {
 
-    private String CLIENT_ID;
-	private String CLIENT_SECRET;
-	private String PROPERTIES_FILENAME = "application.properties";
+    static private String CLIENT_ID;
+    static private String CLIENT_SECRET;
+    static private String AUTH_URL;
+    static private String API_URL;
+    static private String PROPERTIES_FILENAME = "application.properties";
 
-	public String getCLIENT_ID() {
-		return CLIENT_ID;
-	}
+    static public String getCLIENT_ID() {
+        return CLIENT_ID;
+    }
 
-	public String getCLIENT_SECRET() {
-		return CLIENT_SECRET;
-	}
+    static public String getCLIENT_SECRET() {
+        return CLIENT_SECRET;
+    }
 
-    public void setCLIENT_ID(String CLIENT_ID) {
-        if(CLIENT_ID != null && !CLIENT_ID.isEmpty())
-            this.CLIENT_ID = CLIENT_ID;
+    static public String getAUTH_URL() {
+        return AUTH_URL;
+    }
+
+    static public String getAPI_URL() {
+        return API_URL;
+    }
+
+    static public void setCLIENT_ID(String CLIENT_ID) {
+        if (CLIENT_ID != null && !CLIENT_ID.isEmpty())
+            PropertyLoader.CLIENT_ID = CLIENT_ID;
         else
             throw new IllegalArgumentException("[ERROR] Invalid CLIENT_ID loaded");
-	}
+    }
 
-    public void setCLIENT_SECRET(String CLIENT_SECRET) {
-        if(CLIENT_SECRET != null && !CLIENT_SECRET.isEmpty())
-            this.CLIENT_SECRET = CLIENT_SECRET;
+    static public void setCLIENT_SECRET(String CLIENT_SECRET) {
+        if (CLIENT_SECRET != null && !CLIENT_SECRET.isEmpty())
+            PropertyLoader.CLIENT_SECRET = CLIENT_SECRET;
         else
             throw new IllegalArgumentException("[ERROR] Invalid CLIENT_SECRET loaded");
-	}
+    }
 
-    PropertyLoader() {
+    static public void setAUTH_URL(String AUTH_URL) {
+        if (AUTH_URL != null && !AUTH_URL.isEmpty())
+            PropertyLoader.AUTH_URL = AUTH_URL;
+        else
+            throw new IllegalArgumentException("[ERROR] Invalid AUTH_URL loaded");
+    }
+
+    static public void setAPI_URL(String API_URL) {
+        if (API_URL != null && !AUTH_URL.isEmpty())
+            PropertyLoader.API_URL = API_URL;
+        else
+            throw new IllegalArgumentException("[ERROR] Invalid API_URL loaded");
+    }
+
+    static public void loadProperties() {
         Properties properties = new Properties();
         InputStream inputStream = PropertyLoader.class.getClassLoader().getResourceAsStream(PROPERTIES_FILENAME);
 
         try {
-			properties.load(inputStream);
+            properties.load(inputStream);
             setCLIENT_ID(properties.getProperty("CLIENT_ID"));
             setCLIENT_SECRET(properties.getProperty("CLIENT_SECRET"));
-		} catch (IOException e) {
+            setAUTH_URL(properties.getProperty("AUTH_URL"));
+            setAPI_URL(properties.getProperty("API_URL"));
+        } catch (IOException e) {
             System.out.println(e);
         } catch (IllegalArgumentException e) {
             System.out.println(e);
