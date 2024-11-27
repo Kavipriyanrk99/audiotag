a8d8e883a788cc863b881573e7ca2d5d5832c381
Kavipriyan R K <kavipriyanrk99@gmail.com>
2024-11-27 03:23:06 +0000

    metadata received from API

--- src/main/java/com/kavipriyanrk99/audiotag/Metadata.java
+++ src/main/java/com/kavipriyanrk99/audiotag/Metadata.java
@@ -1,28 +1,104 @@
 package com.kavipriyanrk99.audiotag;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-import java.util.Set;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpRequest.BodyPublisher;
+import java.net.http.HttpRequest.BodyPublishers;
 
-import org.json.JSONException;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 /**
  * Metadata
  */
 public class Metadata {
+    void getMetadata(String searchTerm) {
+        APIHandler.setAccessToken();
+        String accessToken = APIHandler.getAccessToken();
+        String tokenType = APIHandler.getTokenType();
 
-    public static void main(String[] args) {
+        if (accessToken == null || accessToken.isEmpty())
+            throw new IllegalArgumentException("[ERROR] Invalid access token");
+
+        if (tokenType == null || tokenType.isEmpty())
+            throw new IllegalArgumentException("[ERROR] Invalid token type");
+
         try {
+            String uri = String.format("%sv1/search?q=%s&type=%s&limit=%s", PropertyLoader.getAPI_URL(),
+                    searchTerm.replace(' ', '+'), "track", "5");
+            URI requestUri = new URI(uri);
+            String token = tokenType + " " + accessToken;
+            String contentType = "";
+            String method = "GET";
+            BodyPublisher bodyPublisher = BodyPublishers.noBody();
+            String responseData = APIHandler.makeHttpRequest(requestUri, token, contentType, method, bodyPublisher);
 
-            JSONObject jo = new JSONObject(
-                    "{\"city\":\"chicago\",\"name\":\"jon doe\",\"age\":\"22\"}");
-            System.out.println(jo.get("city"));
-            System.out.println(jo.get("citys"));
-        } catch (JSONException e) {
+            if (responseData == null || responseData.isEmpty())
+                throw new IllegalStateException("[ERROR] Invalid reponse data");
+
+            JSONObject data = new JSONObject(responseData);
+            JSONObject tracks = (JSONObject) data.get("tracks");
+            JSONArray items = (JSONArray) tracks.get("items");
+
+            for (Object item : items) {
+                if (item instanceof JSONObject) {
+                    JSONObject jsonObjItem = null;
+                    jsonObjItem = (JSONObject) item;
+                    JSONObject album = (JSONObject) jsonObjItem.get("album");
+                    JSONArray images = (JSONArray) album.get("images");
+                    JSONArray artists = (JSONArray) jsonObjItem.get("artists");
+
+                    // [x] Title "name": "Blood // Water",
+                    // [x] Artist "name": "grandson"
+                    // [x] Album "name": "Blood // Water"
+                    // AlbumArtist "name": "grandson"
+                    // [x] Year
+                    // [x] Date "release_date": "2018-05-04"
+                    // Genre
+                    // Lyrics
+                    // Composer
+                    // Publisher
+                    // [x] AlbumImage
+
+                    String title = (String) jsonObjItem.get("name");
+                    String albumName = (String) album.get("name");
+                    String releaseDate = (String) album.get("release_date");
+                    String releaseYear = releaseDate.substring(0, releaseDate.indexOf('-'));
+                    String artistName = null;
+                    String albumImageURL = (String)((JSONObject)images.get(1)).get("url");
+
+                    for (Object jsonObjArtist : artists) {
+                        if (jsonObjArtist instanceof JSONObject) {
+                            JSONObject artist = (JSONObject) jsonObjArtist;
+                            if (artistName == null)
+                                artistName = (String) artist.get("name");
+                            else
+                                artistName = artistName + ", " + artist.get("name");
+                        } else
+                            throw new IllegalStateException("[ERROR] Invalid JSON data");
+                    }
+
+                    System.out.println(title + " " + albumName + " " + albumImageURL +  " " + releaseDate + " " + releaseYear + " " + artistName);
+                } else
+                    throw new IllegalStateException("[ERROR] Invalid JSON data");
+            }
+        } catch (URISyntaxException e) {
+            System.out.println(e);
+        } catch (IOException e) {
+            System.out.println(e);
+        } catch (InterruptedException e) {
+            System.out.println(e);
+        } catch (IllegalStateException e) {
             System.out.println(e);
         }
     }
 
+    void tagger() {
+
+    }
+
+    public static void main(String[] args) {
+        new Metadata().getMetadata("hey minnaley");
+    }
 }
